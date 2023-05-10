a81c0f242936242d35217c63882b6d135af7434f
EagleVector <kumarsubhambili3@gmail.com>
2023-05-10 09:50:18 +0530

    readme added

---
+++ JS_BASICS.js
@@ -0,0 +1,15 @@
+var num = 100;
+
+let marks = 56;
+
+const x = 100;
+
+console.log("Hello world!");
+
+console.log(x);
+
+console.log(marks);
+
+console.log(num);
+
+console.log(10, "start", marks);
