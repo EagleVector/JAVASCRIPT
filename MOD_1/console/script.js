df48a5d2f8e2bdc88da5a719f4364bdfbb6b5871
EagleVector <kumarsubhambili3@gmail.com>
2023-05-15 11:34:19 +0530

    console object in JS

---
+++ MOD_1/console/script.js
@@ -0,0 +1,27 @@
+console.log(100);
+
+console.log("Hello world!");
+
+console.log(15, "helllo", false);
+
+const x = 100;
+
+console.log(x);
+
+console.log(x+10);
+
+console.error(100);
+
+console.warn(x+10);
+
+console.table({name: "Brad", email: "brad@traversy.cc"})
+
+console.group('simple');
+console.log(x);
+console.log(100);
+console.log(true);
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: black; color: white;';
+
+console.log("%cHello World!", styles);
