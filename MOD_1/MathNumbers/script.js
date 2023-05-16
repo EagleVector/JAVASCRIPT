1a6f7a25506b4d03d48d7cb611580a8b0012ccff
EagleVector <kumarsubhambili3@gmail.com>
2023-05-16 12:27:01 +0530

    Math Object

---
+++ MOD_1/MathNumbers/script.js
@@ -0,0 +1,77 @@
+// let x;
+
+// const num = new Number(5.4567);
+
+// x = num.toString();
+
+// x = num.toString().length;
+
+// console.log(num, typeof num);
+
+// console.log(x);
+
+// // x = num.toFixed(2);
+
+// x = num.toExponential(2);
+
+// // x = num.toPrecision(2);
+
+// x = num.valueOf();
+
+// x = Number.MAX_VALUE;
+
+// y = Number.MIN_VALUE;
+
+// console.log(x);
+// console.log(y);
+
+// console.log(num);
+
+// MATH OBJECT
+
+// console.log(Math);
+
+// x = Math.sqrt(9);
+// console.log(x);
+
+// x = Math.abs(-12);
+// console.log(x);
+
+// x = Math.round(7.987);
+// console.log(x);
+
+// x = Math.floor(4.876);
+// console.log(x);
+
+// x = Math.ceil(12.000001);
+// console.log(x);
+
+// x = Math.min(2, 5);
+// console.log(x);
+
+// x = Math.max(1, 3);
+// console.log(x);
+
+// x = Math.random();
+// console.log(x);
+
+// x = Math.random() * 10;
+// console.log(x);
+
+let x = Math.ceil(Math.random() * 100);
+let y = Math.ceil(Math.random() * 50);
+
+sumOutput = `Sum of ${x} and ${y} is ${x + y}`;
+console.log(sumOutput);
+
+diffOutput = `Difference of ${x} and ${y} is ${x - y}`;
+console.log(diffOutput);
+
+prodOutput = `Product of ${x} and ${y} is ${x * y}`;
+console.log(prodOutput);
+
+quotientOutput = `Quotient of ${x} and ${y} is ${x / y}`;
+console.log(quotientOutput);
+
+remOutput = `Remainder of ${x} and ${y} is ${x % y}`;
+console.log(remOutput);
