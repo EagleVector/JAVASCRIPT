46ed6dd6b632dde97a62f4b89f90dd0934829792
EagleVector <kumarsubhambili3@gmail.com>
2023-10-16 23:21:50 +0530

    linked list custom implementation and problem solving

--- traversy-js-challenges/07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
+++ traversy-js-challenges/07-stacks-queues-linked-lists/11-reverse-string-linked-list/reverse-string-linked-list.js
@@ -1,5 +1,21 @@
 const { LinkedList } = require('./linked-list');
 
-function reverseStringLinkedList() {}
+function reverseStringLinkedList(str) {
+  const LL = new LinkedList();
+  let revStr = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    LL.add(str[i]);
+  }
+
+  let current = LL.head;
+
+  while (current !== null) {
+    revStr = revStr + current.data;
+    current = current.next;
+  }
+  
+  return revStr;
+}
 
 module.exports = reverseStringLinkedList;
