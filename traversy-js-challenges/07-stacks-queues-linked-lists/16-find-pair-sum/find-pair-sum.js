463318c68b07478b647acc3aba93aff549dcf0da
EagleVector <kumarsubhambili3@gmail.com>
2023-10-17 16:21:29 +0530

    Doubly linked list problem solving

--- traversy-js-challenges/07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
+++ traversy-js-challenges/07-stacks-queues-linked-lists/16-find-pair-sum/find-pair-sum.js
@@ -1,5 +1,18 @@
 const DoublyLinkedList = require('./DoublyLinkedList');
 
-function findPairSum() {}
+function findPairSum(nums, targetSum) {
+  const list = new DoublyLinkedList();
+
+  for (const num of nums) {
+    const difference = targetSum - num;
+
+    if (list.contains(difference)) {
+      return [difference, num];
+    }
+    list.append(num);
+  }
+
+  return null;
+}
 
 module.exports = findPairSum;
