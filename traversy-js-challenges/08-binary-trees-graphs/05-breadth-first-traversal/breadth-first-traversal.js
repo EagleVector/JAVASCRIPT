6d0521525df9e9f709b190e8f4d26818eb7421fe
EagleVector <kumarsubhambili3@gmail.com>
2023-10-18 17:25:19 +0530

    Breadth First Search Traversal with queue

--- traversy-js-challenges/08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ traversy-js-challenges/08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,8 +1,46 @@
 const Queue = require('./queue');
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(root) {
+
+  const queue = new Queue();
+  
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  // const queue = [];
+
+  // queue.push(root);
+  queue.enqueue(root);
+
+  // while (queue.length > 0) {
+  //   const current = queue.shift();
+  //   result.push(current.data);
+  while (!queue.isEmpty()) {
+    const current = queue.dequeue();
+    result.push(current.data);
+    
+    if (current.left) {
+      // queue.push(current.left);
+      queue.enqueue(current.left);
+    }
+
+    if (current.right) {
+      // queue.push(current.right);
+      queue.enqueue(current.right);
+    }
+  }
+  return result;
+}
 
 module.exports = {
   Node,
