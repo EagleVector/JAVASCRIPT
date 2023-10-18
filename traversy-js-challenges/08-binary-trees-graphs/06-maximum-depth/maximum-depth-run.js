41175f628581039b72a6a28ab43304431ea49872
EagleVector <kumarsubhambili3@gmail.com>
2023-10-18 17:41:59 +0530

    depth of binary tree

--- traversy-js-challenges/08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
+++ traversy-js-challenges/08-binary-trees-graphs/06-maximum-depth/maximum-depth-run.js
@@ -5,3 +5,16 @@ const { Node, maxDepth } = require('./maximum-depth');
 //                          9  20
 //                             / \
 //                            15  7
+
+const A = new Node(3);
+const B = new Node(9);
+const C = new Node(20);
+const D = new Node(15);
+const E = new Node(7);
+
+A.left = B;
+A.right = C;
+C.left = D;
+C.right = E;
+
+console.log(maxDepth(A));
