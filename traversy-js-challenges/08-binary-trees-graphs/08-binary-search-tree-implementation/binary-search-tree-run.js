70faefcbc1759dfaed66b1b1b81901924a3008bc
EagleVector <kumarsubhambili3@gmail.com>
2023-10-19 23:44:19 +0530

    binary search tree implementation

--- traversy-js-challenges/08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
+++ traversy-js-challenges/08-binary-trees-graphs/08-binary-search-tree-implementation/binary-search-tree-run.js
@@ -7,3 +7,20 @@ const { Node, BinarySearchTree } = require('./binary-search-tree');
 //    /
 //   2
 
+const bst = new BinarySearchTree();
+bst.insert(9);
+bst.insert(4);
+bst.insert(11);
+bst.insert(2);
+bst.insert(7);
+bst.insert(15);
+bst.insert(5);
+bst.insert(8);
+
+console.log(bst.lookup(4));
+
+bst.remove(7);
+
+console.log(bst.lookup(4));
+
+bst.printTree();
